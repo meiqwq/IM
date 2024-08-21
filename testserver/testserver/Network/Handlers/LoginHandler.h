e8e465deffde61924180ae1959a671fd71cddc3c
Nobody-or <495298343@qq.com>
2024-08-21 20:07:05 +0800

    Change structure;add team contract

--- testserver/testserver/Network/Handlers/LoginHandler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include"msghandler.h"
-
-class LoginHandler :public MsgHandler {
-	Q_OBJECT
-public:
-    explicit LoginHandler(QObject* parent = nullptr);
-    virtual void parse(Msg& msg, TcpClientSocket* socket = nullptr);
-};
